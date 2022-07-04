ddc5071646fd481ac038e11f8f802804c3ff9c4c
Scarlett Zhang <824834032zs@gmail.com>
2022-07-04 10:44:57 -0400

    display public IP address

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import AddressDisplay from './AddressDisplay';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Banner bannerText="Sextant - Users Internet Dashboard" />
-                <Exhibit name="I'm an exhibit!"></Exhibit>
-                <Exhibit name="I'm also an exhibit!"></Exhibit>
-                <Exhibit name="I'm an exhibit too!"></Exhibit>
+                <Banner bannerText="Sextant - Users Internet Dashboard"/>
+                <Exhibit name="Public IPv4 Address">
+                  <AddressDisplay url='https://api.ipify.org?format=json'/>
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                  <AddressDisplay url='https://api.ipify.org?format=json'/>
+                </Exhibit>
             </div>
         );
     }
